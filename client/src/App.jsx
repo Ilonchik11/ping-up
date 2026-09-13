c352935d8ed1fbbcef1bcca00e9b9f0cecfb59cf
Ilonchik11 <ilonka7011@gmail.com>
2026-09-13 11:16:23 +0300

    Login & Sidebar & Feed Pages: frontend implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
+import { useUser } from "@clerk/react";
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import ChatBox from "./pages/ChatBox";
 import Connections from "./pages/Connections";
 import CreatePost from "./pages/CreatePost";
 import Discover from "./pages/Discover";
 import Feed from "./pages/Feed";
+import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
 
 const App = () => {
+  const { user } = useUser();
+
   return (
     <>
+      <Toaster />
       <Routes>
-        <Route path="/" element={<Login />}>
+        <Route path="/" element={!user ? <Login /> : <Layout />}>
           <Route index element={<Feed />} />
           <Route path="messages" element={<Messages />} />
           <Route path="messages/:userId" element={<ChatBox />} />
